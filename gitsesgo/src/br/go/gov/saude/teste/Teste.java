eadd4d88b2e9a47e66d17e96861a074ff514ba3c
Rodrigo Melo <rodrigofmelo@gmail.com>
2012-07-31 12:18:26 -0300

    add commentário da classe. add método main.

--- gitsesgo/src/br/go/gov/saude/teste/Teste.java
+++ gitsesgo/src/br/go/gov/saude/teste/Teste.java
@@ -1,5 +1,12 @@
 package br.go.gov.saude.teste;
 
+/**
+ * @author Rodrigo Freitas Melo
+ *
+ */
 public class Teste {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
 
 }
