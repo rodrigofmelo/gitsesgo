89edf758bbb642fb14dc730cadac5a282a6a3d86
Rodrigo Melo <rodrigofmelo@gmail.com>
2012-08-01 12:20:00 -0300

    Alterações no master branch.

--- gitsesgo/src/br/go/gov/saude/teste/Teste.java
+++ gitsesgo/src/br/go/gov/saude/teste/Teste.java
@@ -1,12 +1,14 @@
 package br.go.gov.saude.teste;
 
 /**
+ * Classe de teste (Master).
+ * 
  * @author Rodrigo Freitas Melo
  *
  */
 public class Teste {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello world");
 	}
 
 }
