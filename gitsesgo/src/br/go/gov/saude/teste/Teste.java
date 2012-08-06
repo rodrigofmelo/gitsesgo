5f6a82e68006d9628d4d6890b7b53297ae698a51
Rodrigo Melo <rodrigofmelo@gmail.com>
2012-08-06 13:18:42 -0300

    add atributo texto.

--- gitsesgo/src/br/go/gov/saude/teste/Teste.java
+++ gitsesgo/src/br/go/gov/saude/teste/Teste.java
@@ -7,11 +7,22 @@ package br.go.gov.saude.teste;
  *
  */
 public class Teste {
+	
+	String texto = "";
+	
 	public static void main(String[] args) {
 		//bug corrigido
 		//bug corrigido 3
 		//Funcionalidade adicionada
 		System.out.println("hello world");
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
 	}
 
 }
